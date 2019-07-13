8ed5c8ba9f642288f25f1f5379b9df0bbd9bf446
anyuzx <stefanshi1988@gmail.com>
2019-07-12 23:01:48 -0500

    add facebook/twitter/whatsapp/weibo icons

--- plugins/fontawesome.js
+++ plugins/fontawesome.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faAngleDoubleDown, faEllipsisH, faShareAltSquare } from '@fortawesome/free-solid-svg-icons'
-import { faGithub, faInstagram, faWordpressSimple, faResearchgate, faFlickr, faZhihu, faMediumM } from '@fortawesome/free-brands-svg-icons'
+import { faWhatsapp, faFacebookF, faWeibo, faTwitter, faGithub, faInstagram, faWordpressSimple, faResearchgate, faFlickr, faZhihu, faMediumM } from '@fortawesome/free-brands-svg-icons'
 import { faSave, faPaperPlane, faEnvelope } from '@fortawesome/free-regular-svg-icons'
 
 import { faGoogleScholarSquare, faGoogleScholar, faResearchGateSquare, faResearchGate } from '~/assets/customicons'
@@ -12,7 +12,7 @@ config.autoAddCss = false
 
 // You can add your icons directly in this plugin. See other examples for how you
 // can add other styles or just individual icons.
-library.add(faAngleDoubleDown, faEllipsisH, faShareAltSquare, faGithub, faInstagram, faWordpressSimple, faResearchgate, faFlickr, faZhihu, faMediumM, faSave, faPaperPlane, faEnvelope, faGoogleScholarSquare, faGoogleScholar, faResearchGateSquare, faResearchGate)
+library.add(faAngleDoubleDown, faEllipsisH, faShareAltSquare, faWhatsapp, faFacebookF, faWeibo, faTwitter, faGithub, faInstagram, faWordpressSimple, faResearchgate, faFlickr, faZhihu, faMediumM, faSave, faPaperPlane, faEnvelope, faGoogleScholarSquare, faGoogleScholar, faResearchGateSquare, faResearchGate)
 
 // Register the component globally
 Vue.component('font-awesome-icon', FontAwesomeIcon)
