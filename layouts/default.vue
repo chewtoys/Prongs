93b0400be514d3928fc9e4065b3d338d4de3b9cd
anyuzx <stefanshi1988@gmail.com>
2019-07-13 20:18:52 -0500

    update default layout to include navbar and footer

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,27 @@
 <template>
-  <div>
-    <nuxt />
-  </div>
+<div id="post">
+  <v-navbar v-bind:class="{'is-fixed-top':hasStickyNavbar}"></v-navbar>
+  <nuxt />
+  <v-footer></v-footer>
+</div>
 </template>
+
+<script>
+import TheNavbar from '~/components/TheNavbar';
+import TheFooter from '~/components/TheFooter';
+
+export default {
+  components: {
+    'v-navbar': TheNavbar,
+    'v-footer': TheFooter
+  },
+  data () {
+    return {
+      hasStickyNavbar: true
+    }
+  },
+  mounted () {
+    document.body.classList.toggle('fixed', this.hasStickyNavbar);
+  }
+}
+</script>
